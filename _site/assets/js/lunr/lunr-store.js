e6f2d132ee2d6369c4e56769c832fc948941992e
Ryan Remaly <remalyryan@gmail.com>
2020-12-22 12:45:43 -0500

    Adding posts

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,19 @@
-var store = []
+var store = [{
+        "title": "Hello World",
+        "excerpt":"Hello whoever is reading this. I realistically should have done this many years ago but you know what they say about planting a tree. If I said I had a complete vision for how this blog/portfolio would end up I’d be lying. I figure if I’m working on something I...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/firstPost/",
+        "teaser": null
+      },{
+        "title": "My iPad Experience",
+        "excerpt":"In the summer of 2020, I purchased a 12.9 inch iPad pro after discovering the feature sidecar. For those unfamiliar, sidecar allows you to use your iPad as a second monitor for your macbook with or without a wire. Considering the amount of developement I had been doing on Global...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/MyiPadExperience/",
+        "teaser": null
+      },{
+        "title": "A Happy Accident",
+        "excerpt":"Life sometimes has the ability to throw a few curveballs, especially when things seem to settle down. On a cold morning in February 2019 I was doing what I had done for the last 6 months. Drive to work. Only this morning was a little different because there was more...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/A-Happy-Accident/",
+        "teaser": null
+      }]
