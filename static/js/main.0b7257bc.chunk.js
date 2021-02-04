d14cb94b50f6da373bd80716f8957951b7e183c5
Ryan Remaly <remalyryan@gmail.com>
2021-02-04 13:58:35 -0500

    Updates

--- static/js/main.0b7257bc.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpsite=this.webpackJsonpsite||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);n(1);var s=n(3),c=n.n(s),i=(n(8),n(0));var r=function(){return Object(i.jsx)("div",{className:"App",children:Object(i.jsx)("h1",{children:"Yo"})})};c.a.render(Object(i.jsx)(r,{}),document.getElementById("root"))},8:function(e,t,n){}},[[10,1,2]]]);
-//# sourceMappingURL=main.0b7257bc.chunk.js.map
